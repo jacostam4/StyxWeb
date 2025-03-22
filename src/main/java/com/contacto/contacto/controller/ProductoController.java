7e6e25d6571f9c129df9c6359a78226e9c634ed1
Juanito_alimañas <75868875+jacostam4@users.noreply.github.com>
2025-03-22 09:51:38 -0500

    CRUD de productos elaborado

--- src/main/java/com/contacto/contacto/controller/ProductoController.java
+++ src/main/java/com/contacto/contacto/controller/ProductoController.java
@@ -6,9 +6,17 @@ import org.springframework.web.bind.annotation.RestController;
 import com.contacto.contacto.model.ProductoModel;
 import com.contacto.contacto.service.ProductoService;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
 
 
 
@@ -26,6 +34,21 @@ public class ProductoController {
     @PostMapping("/register")
     public ResponseEntity<ProductoModel> register(@RequestBody ProductoModel producto) {
         return ResponseEntity.ok(productoService.register(producto));
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<ProductoModel>> getAllProductos() {
+        return ResponseEntity.ok(productoService.getAllProductos());
+    }
+    
+    @PutMapping("/update/{id}")
+    public ResponseEntity<ProductoModel> updateProducto(@PathVariable("id") long id,@RequestBody ProductoModel producto) {
+        return ResponseEntity.ok(productoService.updateProducto(id, producto));
+    }
+
+    @GetMapping("/search/{id}")
+    public ResponseEntity<ProductoModel> getProductoById(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(productoService.getProductoById(id));
     }
     
 }
--- src/main/java/com/contacto/contacto/service/ProductoService.java
+++ src/main/java/com/contacto/contacto/service/ProductoService.java
@@ -1,5 +1,8 @@
 package com.contacto.contacto.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import com.contacto.contacto.model.ProductoModel;
@@ -12,8 +15,36 @@ public class ProductoService {
     public ProductoService(ProductoRepository productoRepository) {
         this.productoRepository = productoRepository;
     }
-
+    //Registrar un producto
     public ProductoModel register(ProductoModel producto) {
         return productoRepository.save(producto);
+    }
+
+    //Obtener todos los productos
+    public List<ProductoModel> getAllProductos() {
+        return productoRepository.findAll();
+    }
+
+    //actualizar productos
+    public ProductoModel updateProducto (Long id, ProductoModel producto) {
+        return productoRepository.findById(id).map(existeProducto -> {
+            existeProducto.setNombre(producto.getNombre());
+            existeProducto.setPrecio(producto.getPrecio());
+            existeProducto.setReferencia(producto.getReferencia());
+            existeProducto.setCantidad(producto.getCantidad());
+            existeProducto.setCosto(producto.getCosto());
+            existeProducto.setImagenes(producto.getImagenes());
+            return productoRepository.save(existeProducto);
+        }).orElseThrow(() -> new RuntimeException("Producto no encontrado"));   
+    }
+
+    public ProductoModel getProductoById(Long id) {
+        Optional<ProductoModel> producto = productoRepository.findById(id);
+        if (producto.isPresent()) {
+            return producto.get();
+        }
+        else {
+            throw new RuntimeException("Producto no encontrado");
+        }
     }
 }
